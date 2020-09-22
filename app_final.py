2b4cb26cb871aaf3cecfa69c6f2eb08faf28f797
Chamsedine AIDARA <50946856+chamsdg@users.noreply.github.com>
2020-09-22 15:07:52 +0200

    Update app_final.py

--- app_final.py
+++ app_final.py
@@ -13,7 +13,7 @@ import seaborn as sns
 import io
 #import os
 #import matplotlib
-import numpy as np
+#import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder 
